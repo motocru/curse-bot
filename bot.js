2a0fb5a25f0d705929a21c8a726afefaf70e5f1d
Kevin Urban <urban.kevi@gmail.com>
2019-03-24 09:26:32 -0500

    updated for server db re-work

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var logger = require('winston');
 var auth = require('./auth.json');
 var curses = require('./curses.json');
 var users = require('./db/users');
+var servers = require('./db/servers');
 
 /**adding the  message constants below */
 const USERMILESTONES = {
@@ -71,14 +72,14 @@ bot.on('message', function(user, userID, channelID, message, evt) {
    */
   if (userID === bot.id) return;
   if (message.substring(0,1) == '?') {
-    botCommands(message, evt, function(response) {
+    botCommands(message, evt, channelID, function(response) {
       bot.sendMessage({
         to: channelID,
         message: response
       });
     });
   } else {
-    parseMessage(userID, channelID, message);
+    parseMessage(userID, channelID, message, evt.d.guild_id);
   }
 });
 
@@ -86,7 +87,7 @@ bot.on('message', function(user, userID, channelID, message, evt) {
  * treated as commands for curse-bot from code that parses normal messages
  * for curse words
  */
-function botCommands(message, evt, cb) {
+function botCommands(message, evt, channelID, cb) {
   var args = message.substring(1).split(' ');
   var cmd = args[0].toUpperCase();
 
@@ -94,18 +95,40 @@ function botCommands(message, evt, cb) {
     case "HELP":
       cb(curses.helpMessage);
       break;
-    case "TOTAL":
+    case "COUNT":
       if (evt.d.mentions[0] === undefined) {
-        serverAndUserSwearTotals(null, null, function(completed) {
-          cb(completed);
-        });
+        countResponseMessage(null, null, evt.d.guild_id, cb);
       } else {
         for (var i in evt.d.mentions) {
-          serverAndUserSwearTotals(evt.d.mentions[i].id, (evt.d.mentions[i].member.nick != undefined) ? evt.d.mentions[i].member.nick : evt.d.mentions[i].username, function(completed) {
-            cb(completed);
-          });
+          countResponseMessage(evt.d.mentions[i].id, (evt.d.mentions[i].member.nick !== undefined) ? evt.d.mentions[i].member.nick : evt.d.mentions[i].username, evt.d.guild_id, cb);
         }
       }
+      break;  
+    case "TOTAL":
+      if (evt.d.mentions[0] === undefined) {
+        serverAndUserSwearList(null, null, evt.d.guild_id, cb);
+      } else {
+        for (var i in evt.d.mentions) {
+          serverAndUserSwearList(evt.d.mentions[i].id, (evt.d.mentions[i].member.nick !== undefined) ? evt.d.mentions[i].member.nick : evt.d.mentions[i].username, evt.d.guild_id, cb);
+        }
+      }
+      break;
+    case "FIRST":
+      servers.returnSealBreaker(evt.d.guild_id, function(err, user) {
+        if (user === null) {
+          cb('No on this server has cursed yet');
+        } else {
+          cb(`<@!${user.id}> ${curses.sealBreakerMessage}`)
+        }
+      });
+      break;  
+    case "TEST":
+      //console.log(evt);
+      //console.log(channelID);
+      bot.sendMessage({
+        to: channelID,
+        message: `<@!${evt.d.guild_id}>`
+      });
       break;
     default :
       cb("Unkown Command, try using '?help'");
@@ -115,14 +138,15 @@ function botCommands(message, evt, cb) {
 
 /**this function is needed as the 'else' in the initial if-else statement 
  * for better code readability on the initial message. if message has any
- * curse-words involved it sends to the next function, otherwise it returns
+ * curse-words involved it sends our array of curse words to the next function, 
+ * otherwise it returns
  */
-function parseMessage(userID, channelID, message) {
+function parseMessage(userID, channelID, message, guild) {
   message = message.toUpperCase();
   message = message.replace(/[.,\/#!$'"%?\^&\*;:{}=\-_`~()\[\]]/g,' ');
   words = message.match(new RegExp(`\\b(${curses.curses.join("|")})\\b`,"gi"));
   if (words !== null) {
-    incrementSwearsAndSendMessage(words, userID, channelID);
+    incrementSwearsAndSendMessage(words, userID, channelID, guild);
   } else {
     return;
   }
@@ -131,14 +155,15 @@ function parseMessage(userID, channelID, message) {
 /**function needed as the previous solution was to have a large if statement
  * with no else involved. now if a message has curse words it is sent here
  */
-function incrementSwearsAndSendMessage(words, userID, channelID) {
-  users.save(userID, {}, function(err, user) {
+function incrementSwearsAndSendMessage(words, userID, channelID, guild) {
+  /**new version */
+  servers.saveServer(guild, userID, {}, function(err, user) {
     if (err) {console.log(err);}
     var addedObject = {};
     if (JSON.stringify(user.jarObject) === '{}') {
       bot.sendMessage({
         to: channelID,
-        message: `<@!${userID} ${curses.userMessages.message1}>`
+        message: `<@!${userID}> ${curses.userMessages.message1}`
       });
     }
     for (var i in words) {
@@ -155,9 +180,9 @@ function incrementSwearsAndSendMessage(words, userID, channelID) {
       }
     }
 
-    users.addSwear(userID, user.jarObject, function(err2, updatedUser) {
+    servers.updateUserSwears(guild, userID, user.jarObject, function(err2, updatedUser) {
       messageEvaluator(updatedUser, addedObject, function(printMessage1) {
-        users.totalSwears(function(err, total) {
+        servers.totalServerSwears(guild, function(err2, total) {
           serverEvaluator(total, addedObject, function(printMessage2) {
             bot.sendMessage({
               to: channelID,
@@ -173,37 +198,73 @@ function incrementSwearsAndSendMessage(words, userID, channelID) {
 /**this function is called when the bot is given the '?total' command. The function is
  * used to build up user and server totals on a per-swear basis
  */
-function serverAndUserSwearTotals(user, nickname, cb) {
-  var returnString = (user == null) ? `Server total swear count\n` : `${nickname} total swear count\n`;
-  if (user == null) {
-    users.totalSwears(function(err, swearObject) {
-      curseTotalStringBuilder(swearObject, returnString, function(completed) {
-        cb(completed);
-      })
+function serverAndUserSwearList(user, nickname, guild, cb) {
+  var returnString = (user === null) ? `Server total swear count\n` : `${nickname} total swear count\n`;
+  if (user === null) {
+    servers.totalServerSwears(guild, function(err, curses) {
+      if (curses === null) cb(returnString+'This server has not cursed yet');
+      else {
+        sortCurseList(curses, function(sorted) {
+          curseTotalStringBuilder(sorted, returnString, function(completed) {
+            cb(completed);
+          });
+        });
+      }
     });
   } else {
-    users.userSwears(user, function(err, curseObject) {
+    users.getUserServerSwearList(guild, user, function(err, curseObject) {
       if (err) {console.log(err);}
       if (curseObject == null) {
         cb(returnString.concat(`${nickname} ${curses.curseFreeMessage}`));
       } else {
-        curseTotalStringBuilder(curseObject, returnString, function(completed) {
-          cb(completed);
+        sortCurseList(curseObject, function(sorted) {
+          curseTotalStringBuilder(sorted, returnString, function(completed) {
+            cb(completed);
+          });
         });
       }
     });
   }
 }
 
+function sortCurseList(curses, cb) {
+  var sorted = [];
+  for (var curse in curses) {
+    sorted.push([curse, curses[curse]]);
+  }
+  sorted.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+  cb(sorted);
+}
+
 /**builds a string by looping over every given item in a curse object for both a 
  * total server count and total user count to reduce redundant code rather than 
  * having the same thing twice for server and user
 */
-function curseTotalStringBuilder(curseObject, responseString, cb) {
-  for (var i in curseObject) {
-    responseString = responseString.concat(`${i.toLowerCase()}: ${curseObject[i]}\n`);
+function curseTotalStringBuilder(curseArray, responseString, cb) {
+  for (var i in curseArray) {
+    responseString = responseString.concat(`${curseArray[i][0].toLowerCase()}: ${curseArray[i][1]}\n`);
   }
   cb(responseString);
+}
+
+/**returns the total number of times the server or a single user has
+ * used any curse words
+ */
+function countResponseMessage(user, nickname, guild, cb) {
+  var responseString = (user === null) ? `Total times this server has swore:\n` : `Total times ${nickname} has swore:\n`;
+  if (user === null) {
+    servers.serverSwearCount(guild, function(err, count) {
+      if (count === 0) cb(responseString+'No one has cursed on this server yet');
+      else cb(responseString+count);
+    });
+  } else {
+    users.swearCountByServer(guild, user, function(err, count) {
+      if (count === 0) cb(responseString+`${nickname} has not cursed on this server yet`);
+      else cb(responseString+count);
+    });
+  }
 }
 
 /** This function is needed to build a result string to send in a callback
@@ -236,12 +297,11 @@ function serverEvaluator(total, addedObject, cb) {
  * crosses a threshold for a message then returns the message or nothing if there isn't one.
  */
 function messageDecider(user, curse, difference, MILESTONES, cb) {
-  /**rework here */
-  if (user._id === undefined) {
+  if (user.id === undefined) {
     addressorText = 'This server';
     curseNum = user[curse];
   } else {
-    addressorText = `<@!${user._id}>`;
+    addressorText = `<@!${user.id}>`;
     curseNum = user.jarObject[curse]
   }
 
