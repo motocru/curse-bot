d8673ccfe5bc3403a99e5e39c7359e7692fc9f01
Kevin Urban <urban.kevi@gmail.com>
2026-02-22 09:13:01 -0600

    fixed swear editor role permissions

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,3 +1,4 @@
+import { notStrictEqual } from "assert";
 import { GuildMember } from "discord.js";
 
 const SWEAR_EDITOR_ROLE = 'SWEAR EDITOR'
@@ -6,7 +7,12 @@ export async function canEditSwearList(member: GuildMember): Promise<boolean> {
     //I AM THE OWNER
     if (member.user.id === '345968473618382850') return true;
 
-    if (member.roles.cache.some(role => role.name === SWEAR_EDITOR_ROLE)) return true;
-    var role = await member.guild.roles.fetch(SWEAR_EDITOR_ROLE);
-    return role !== null;
+    const user = await member.guild?.members.fetch(member.user.id);
+    for (const role of user?.roles.valueOf() ?? []) {
+        const serverRole = await member.guild?.roles.fetch(role[0]);
+        if (serverRole?.name?.toLocaleUpperCase() == SWEAR_EDITOR_ROLE) {
+            return true;
+        }
+    }
+    return false
 }
